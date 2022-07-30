9eb6d1ac5692d1a0a8dff66e48438e5f120e479d
Guistopa <102003452+Guistopa@users.noreply.github.com>
2022-07-29 21:21:00 -0300

    Abstração RPG Disfuncional
    Entendimento e Aplicação das metodologias desenvolvidas e trabalhadas

---
+++ Dio - Abstração_RPG/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Projeto1GFT.src.Models;
+
+
+namespace projeto1gft
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Wizard wizard = new Wizard("Carvão", 76, "Wizard");
+            Archer archer = new Archer("Thor", 78, "Archer");
+            Beserker beserker = new Beserker("Brutos", 81, "Beserker");
+            Knight knight = new Knight("Hulk", 81, "Knight");
+            Priest priest = new Priest("Jhonas", 80, "Priest");
+
+        
+            Console.WriteLine(wizard.Attack());
+            Console.WriteLine(archer.Attack(8));
+            Console.WriteLine(beserker.Attack(8));
+            Console.WriteLine(knight.Attack());
+            Console.WriteLine(priest.Attack(6));
+        
+        }
+
+    }
+}
+
+
+
+
---
+++ Dio - Abstração_RPG/README.md
@@ -0,0 +1,2 @@
+#Projet baseado na aula e buscando implementar os conceitos 
+#Os Arquivos são feito upload devido a restrição do computador e rede
